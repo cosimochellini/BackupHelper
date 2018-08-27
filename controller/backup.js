7e8869e6d06d1daf39f5f03ac6b14682125b209e
INLDOM\c.chellini <c.chellini@inlinea.net>
2018-08-27 16:07:14 +0200

    modifiche al settings.json

---
+++ controller/backup.js
@@ -0,0 +1,10 @@
+const fse = require('fs-extra')
+
+let backupController = {};
+
+
+backupController.execute = (settings, dirname) => {
+
+
+}
+
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
-const rimborsoSpese = require("./controller/rimborsoSpese");
+const backupController = require("./controller/backup");
 
 const settings = JSON.parse(fs.readFileSync('./settings/settings.json'));
 
-rimborsoSpese.execute(settings, __dirname)
+backupController.execute(settings, __dirname)
     .then(() => process.stdout.write("script runned correctly\n"))
     .catch(error => process.stdout.write(error));
