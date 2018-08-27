bf9b9082f9b19a1a9f4e60118e33a7f1d4d4f5a1
INLDOM\c.chellini <c.chellini@inlinea.net>
2018-08-27 15:53:05 +0200

    startup

---
+++ server.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const rimborsoSpese = require("./controller/rimborsoSpese");
+
+const settings = JSON.parse(fs.readFileSync('./settings/settings.json'));
+
+rimborsoSpese.execute(settings, __dirname)
+    .then(() => process.stdout.write("script runned correctly\n"))
+    .catch(error => process.stdout.write(error));
